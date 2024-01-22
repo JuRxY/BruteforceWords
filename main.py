5bfa3f6549b3fbaf94ab624a33dbfc9541223a29
Jure <grcar.jure1@gmail.com>
2024-01-22 23:26:32 +0100

    initial

---
+++ main.py
@@ -0,0 +1,34 @@
+import itertools
+import math
+
+
+def generate(list, word_length):
+    yield from itertools.permutations(list, word_length)
+
+def generate_possibilities(letters: str):
+    open("possibilities.txt", "w").close()
+
+    slovar = []
+    with open('sbsj.txt', 'r', encoding='utf-8') as f:
+        line = f.readline()
+        slovar.append(line.strip())
+
+    possibilities_list = []
+    length = len(letters)
+    total_possibilities = sum(math.factorial(length) // math.factorial(length - i) for i in range(3, length + 1))
+    print(f"There are `{total_possibilities}` possibilities")
+    #with open("wordlist.txt", "r") as f:
+    #    global seznam_besed
+    #    seznam_besed = f.read().splitlines()
+
+    for i in range(3, length + 1):
+        for j in generate(letters, i):
+            word = "".join(j)
+            possibilities_list.append(word) #! ig da to converta use tuple u en list povn stringou
+            #with open("possibilities.txt", "a") as f:
+            #    f.write("".join(j) + "\n")
+
+    return list(set(possibilities_list)-(set(possibilities_list) - set(slovar)))
+
+pos = generate_possibilities("rkao")
+print(pos)
